f049ebe97f1ffc8eab75e4f75333575f883b47d0
myar-dev <55045364+myar-dev@users.noreply.github.com>
2019-12-03 14:43:54 +0300

    ViewModel added

---
+++ app/src/main/java/com/gcs/appmaterials/viewmodel/ArticleViewModel.java
@@ -0,0 +1,33 @@
+package com.gcs.appmaterials.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.gcs.appmaterials.data.ArticleRepository;
+import com.gcs.appmaterials.model.Article;
+
+import java.util.List;
+
+public class ArticleViewModel extends AndroidViewModel {
+
+    private ArticleRepository repository;
+    private LiveData<List<Article>> localArticles;
+
+    public ArticleViewModel(@NonNull Application application) {
+        super(application);
+        repository = new ArticleRepository(application);
+        localArticles = repository.getLocalArticles();
+    }
+
+    public LiveData<List<Article>> getLocalArticles(){
+        return localArticles;
+    }
+
+    public List<Article> getById(int...ids){
+        return repository.getArticlesByIds(ids);
+    }
+
+}
